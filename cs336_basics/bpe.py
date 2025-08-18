383cf96fecee986525e7b733ab9ad98a27b524d5
xing.mei <xing.mei@bytedance.com>
2025-08-18 16:58:39 -0700

    initial train_bpe

---
+++ cs336_basics/bpe.py
@@ -0,0 +1,70 @@
+import os
+import regex as re
+
+ENCODE = "utf-8"
+
+def train_bpe(
+    input_path: str | os.PathLike,
+    vocab_size: int,
+    special_tokens: list[str],
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    
+    with open(input_path) as f:
+        raw_text = f.read()
+
+        # init vocab, invert_vocab
+        vocab = {} # dict[int, bytes]
+        initial_list = special_tokens + [chr(i) for i in range(256)]
+        for idx, token in enumerate(initial_list):
+            vocab[idx] = bytes(token, ENCODE)
+
+        # split text with special tokens
+        split_pattern = "|".join([re.escape(st) for st in special_tokens])
+        splitted_texts = [text for text in re.split(split_pattern, raw_text) if text]
+
+        # pretokenize each splitted text and put all tokens into a dict
+        token_dict = {}
+        token_pattern = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""        
+        for splitted_text in splitted_texts:
+            for match in re.finditer(token_pattern, splitted_text):
+                token_key = tuple([bytes(c, ENCODE) for c in match.group()])
+                token_dict[token_key] = token_dict.get(token_key, 0) + 1
+
+        # merge pairs, update merge list, vocab, vocab_invert
+        merge_list = []
+        while(len(vocab) < vocab_size):
+            # find pairs with max counts
+            pair_count = {}
+            for token, count in token_dict.items():
+                for pair in zip(token, token[1:]):
+                    pair_count[pair] = pair_count.get(pair, 0) + count
+            pair_with_max_counts = max(pair_count, key=pair_count.get)
+            # update merge list, vocab, vocab_invert
+            merged_id = len(vocab)
+            merged_bytes = pair_with_max_counts[0] + pair_with_max_counts[1]
+            merge_list.append(pair_with_max_counts)
+            vocab[merged_id] = merged_bytes
+            # update token dict
+            new_token_dict = {}
+            for token, count in token_dict.items():
+                if len(token) < 2:
+                    new_token_dict[token] = count
+                else:
+                    new_token = []
+                    i = 0
+                    while i < len(token):
+                        if i < len(token) -1 and (token[i], token[i + 1]) == pair_with_max_counts:
+                            new_token.append(merged_bytes)
+                            i += 2
+                        else:
+                            new_token.append(token[i])
+                            i += 1
+                    new_token = tuple(new_token)
+                    new_token_dict[new_token] = count
+            token_dict = new_token_dict
+        return (vocab, merge_list)
+
+    raise FileNotFoundError(f"The file '{input_path}' was not found.")
+    
+if __name__ == "__main__":
+    train_bpe("data/temp.txt", 259, ["<|endoftext|>"])
--- tests/adapters.py
+++ tests/adapters.py
@@ -9,6 +9,8 @@ import numpy.typing as npt
 import torch
 from torch import Tensor
 
+import cs336_basics
+import cs336_basics.bpe
 
 def run_linear(
     d_in: int,
@@ -589,4 +591,4 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    return cs336_basics.bpe.train_bpe(input_path, vocab_size, special_tokens)
