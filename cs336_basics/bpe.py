bb44a7e410739c1ec43606e4422d1cc4c24a1431
xing-mei <xmei2005@gmail.com>
2025-08-21 00:31:39 -0700

    train_bpe clean up

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -2,10 +2,56 @@ import os
 import regex as re
 import copy
 import time
+from typing import BinaryIO
 from concurrent.futures import ProcessPoolExecutor
-from cs336_basics.pretokenization_example import find_chunk_boundaries
 
 ENCODE = "utf-8"
+
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
 
 def process_file_chunk(
     input_path: str | os.PathLike,
@@ -49,7 +95,6 @@ def train_bpe(
 
     t1 = time.time()
     if use_multiprocess and len(special_tokens) == 1 and special_tokens[0] == "<|endoftext|>":
-        print("path 1")
         # use multiprocess only in this condition
         token_dict = {}
         num_process = 4
@@ -65,7 +110,6 @@ def train_bpe(
             for k, v in local_token_dict.items():
                token_dict[k] = token_dict.get(k, 0) + v
     else:
-        print("path 2")
         with open(input_path, "rb") as f:
             f.seek(0, os.SEEK_END)
             file_size = f.tell()
@@ -126,7 +170,7 @@ def train_bpe(
         del pair_count[pair_with_max_counts]
     t3 = time.time()
 
-    print(f'file loading + pretokenization: {t2 - t1}')
+    print(f'\nfile loading + pretokenization: {t2 - t1}')
     print(f'merging: {t3 - t2}')
     
     return (vocab, merge_list)
